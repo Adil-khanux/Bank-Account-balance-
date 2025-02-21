24338478573bf78e992ecc680fec2756858c19d8
Adil-khanux <8218adilkhan@gmail.com>
2025-02-21 10:38:00 +0530

    new bank account.java

--- new bank account update.java
+++ new bank account update.java
@@ -1,11 +1,6 @@
-/******************************************************************************
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
 
-*******************************************************************************/
-/* k BankAccount class banao jo encapsulation ko follow kare. Usme following functionalities honi chahiye:
+/* BankAccount class banao jo encapsulation ko follow kare. Usme following functionalities honi chahiye:
 
 Private attributes: accountNumber, accountHolderName, balance
 Constructor: Values initialize kare
